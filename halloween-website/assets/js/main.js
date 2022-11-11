c5255ea27d91a9efee594dfcde6d3edb8c66d028
laryssa-ramos <laryssaramos284@gmail.com>
2022-11-10 22:44:29 -0300

    site halloween

--- halloween-website/assets/js/main.js
+++ halloween-website/assets/js/main.js
@@ -52,3 +52,38 @@ function scrollHeader(){
     if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+// NEW SWIPER
+
+let newSwiper = new Swiper(".new-swiper", {
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    spaceBetween: 16,
+    loop:'true',
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+  });
+
+  // SCROLL SECTIONS ACTIVE LINK
+
+  const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
